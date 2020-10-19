f4aedf2df22c74751f05578e848612d71e2e22fd
Eric Clemmons <eric@smarterspam.com>
2020-10-19 15:37:00 -0700

    Amplify.configure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import { Footer } from "components/Footer";
 import "../styles/index.css";
 
+import { Amplify } from "aws-amplify";
+import awsExports from "src/aws-exports";
+
+Amplify.configure(awsExports);
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
