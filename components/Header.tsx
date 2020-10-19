3f74321a7aac2f5c6633d816b295e2b97e2c00a1
Eric Clemmons <eric@smarterspam.com>
2020-10-18 17:27:33 -0700

    Add /posts/[slug].tsx page

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import { Nav } from "components/Nav";
+
+export function Header({ children }) {
+  return (
+    <div className="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+      <Nav />
+
+      <main className="max-w-screen-xl px-4 mx-auto mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+        {children}
+      </main>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Menu, Transition } from "@headlessui/react";
+import { Header } from "components/Header";
+import { Nav } from "components/Nav";
 
-const posts = [
+const placeholderPosts = [
   {
     id: "abc1",
     content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.`.repeat(
@@ -39,7 +40,7 @@ const posts = [
   },
 ];
 
-export default function IndexPage() {
+export default function IndexPage({ posts = placeholderPosts }) {
   return (
     <>
       <div className="relative overflow-hidden bg-gray-50">
@@ -112,165 +113,63 @@ export default function IndexPage() {
           </div>
         </div>
 
-        <div className="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
-          <div className="max-w-screen-xl px-4 mx-auto sm:px-6">
-            <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
-              <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                <div className="flex items-center justify-between w-full md:w-auto">
-                  <a href="https://docs.amplify.aws/" aria-label="Home">
-                    <img
-                      className="w-auto h-8 sm:h-10"
-                      src="https://raw.githubusercontent.com/aws-amplify/aws-amplify.github.io/master/images/Logos/Amplify%20Logo.svg"
-                      alt="Logo"
+        <Header>
+          <div className="text-center">
+            <img
+              className="inline-block w-24 h-24 border-white rounded-full shadow-md md:h-36 md:w-36"
+              src="https://github.com/ericclemmons.png"
+              alt=""
+            />
+
+            <h2 className="mt-3 text-4xl font-extrabold leading-10 tracking-tight text-gray-900 md:mt-5 sm:text-5xl sm:leading-none md:text-6xl">
+              Hi, my name is <br className="xl:hidden" />
+              <span className="text-indigo-600">Eric Clemmons</span>
+            </h2>
+            <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+              I specialize in building <mark>remote teams</mark> & performant{" "}
+              <mark>full stack web applications</mark> with technologies like
+              React, TypeScript, Tailwind CSS, & GraphQL.
+            </p>
+            <div className="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
+              <div className="rounded-md shadow">
+                <a
+                  href="https://twitter.com/ericclemmons"
+                  className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10"
+                >
+                  <svg
+                    className="w-6 h-6 mr-2"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                  </svg>
+                  Twitter
+                </a>
+              </div>
+              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                <a
+                  href="https://github.com/ericclemmons/next.js-conf-2020"
+                  className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10"
+                >
+                  <svg
+                    className="w-6 h-6 mr-2"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                      clipRule="evenodd"
                     />
-                  </a>
-                </div>
-              </div>
-              <div className="md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
-                <Menu>
-                  {({ open }) => (
-                    <>
-                      <span className="rounded-md shadow">
-                        <Menu.Button className="inline-flex items-center px-4 py-2 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-gray-50 active:text-indigo-700">
-                          <span>Log In</span>
-                        </Menu.Button>
-                      </span>
-
-                      <Transition
-                        show={open}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items
-                          static
-                          className="absolute right-0 w-56 mt-8 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
-                        >
-                          <div className="px-4 py-3">
-                            <p className="text-sm leading-5">Signed in as</p>
-                            <p className="text-sm font-medium leading-5 text-gray-900 truncate">
-                              tom@example.com
-                            </p>
-                          </div>
-
-                          <div className="py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#account-settings"
-                                  className={`${
-                                    active
-                                      ? "bg-gray-100 text-gray-900"
-                                      : "text-gray-700"
-                                  } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
-                                >
-                                  Convert to Draft
-                                </a>
-                              )}
-                            </Menu.Item>
-
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#account-settings"
-                                  className={`${
-                                    active
-                                      ? "bg-gray-100 text-gray-900"
-                                      : "text-gray-700"
-                                  } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
-                                >
-                                  Publish
-                                </a>
-                              )}
-                            </Menu.Item>
-                          </div>
-
-                          <div className="py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#account-settings"
-                                  className={`${
-                                    active
-                                      ? "bg-gray-100 text-gray-900"
-                                      : "text-gray-700"
-                                  } flex justify-between w-full px-4 py-2 text-sm leading-5 text-left`}
-                                >
-                                  Sign out
-                                </a>
-                              )}
-                            </Menu.Item>
-                          </div>
-                        </Menu.Items>
-                      </Transition>
-                    </>
-                  )}
-                </Menu>
-              </div>
-            </nav>
-          </div>
-
-          <main className="max-w-screen-xl px-4 mx-auto mt-10 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
-            <div className="text-center">
-              <img
-                className="inline-block w-24 h-24 border-white rounded-full shadow-md md:h-36 md:w-36"
-                src="https://github.com/ericclemmons.png"
-                alt=""
-              />
-
-              <h2 className="mt-3 text-4xl font-extrabold leading-10 tracking-tight text-gray-900 md:mt-5 sm:text-5xl sm:leading-none md:text-6xl">
-                Hi, my name is <br className="xl:hidden" />
-                <span className="text-indigo-600">Eric Clemmons</span>
-              </h2>
-              <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                I specialize in building <mark>remote teams</mark> & performant{" "}
-                <mark>full stack web applications</mark> with technologies like
-                React, TypeScript, Tailwind CSS, & GraphQL.
-              </p>
-              <div className="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
-                <div className="rounded-md shadow">
-                  <a
-                    href="https://twitter.com/ericclemmons"
-                    className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10"
-                  >
-                    <svg
-                      className="w-6 h-6 mr-2"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                    Twitter
-                  </a>
-                </div>
-                <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                  <a
-                    href="https://github.com/ericclemmons/next.js-conf-2020"
-                    className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10"
-                  >
-                    <svg
-                      className="w-6 h-6 mr-2"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                        clipRule="evenodd"
-                      />
-                    </svg>{" "}
-                    GitHub
-                  </a>
-                </div>
+                  </svg>{" "}
+                  GitHub
+                </a>
               </div>
             </div>
-          </main>
-        </div>
+          </div>
+        </Header>
       </div>
 
       <div className="relative px-4 pt-16 pb-20 bg-gray-50 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
@@ -295,7 +194,7 @@ export default function IndexPage() {
                 <div className="flex-shrink-0">
                   <img
                     className="object-cover w-full h-48"
-                    src={`https://source.unsplash.com/featured/480x320/?tech,${post.tags.toString()}`}
+                    src={`https://source.unsplash.com/featured/480x320/?tech,${post.tags}`}
                     alt=""
                   />
                 </div>
@@ -337,50 +236,6 @@ export default function IndexPage() {
           </div>
         </div>
       </div>
-
-      <footer className="bg-white">
-        <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-          <div className="flex justify-center space-x-6 md:order-2">
-            <a
-              href="https://twitter.com/ericclemmons"
-              className="text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">Twitter</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-              </svg>
-            </a>
-            <a
-              href="https://github.com/ericclemmons/next.js-conf-2020"
-              className="text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">GitHub</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </div>
-          <div className="mt-8 md:mt-0 md:order-1">
-            <p className="text-base leading-6 text-center text-gray-400">
-              &copy; 2020 Eric Clemmons. All rights reserved.
-            </p>
-          </div>
-        </div>
-      </footer>
     </>
   );
 }
