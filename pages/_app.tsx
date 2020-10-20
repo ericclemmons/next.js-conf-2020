7a726d2c3e8111a70a35e68503189b8d1810bc1d
Eric Clemmons <eric@smarterspam.com>
2020-10-19 22:31:59 -0700

    Remove Amplify.configure from _app.tsx
    https://github.com/vercel/next.js/issues/16977

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
-import { Amplify } from "aws-amplify";
 import { Footer } from "components/Footer";
-import awsExports from "src/aws-exports";
 import "../styles/index.css";
-
-Amplify.configure(awsExports);
 
 function MyApp({ Component, pageProps }) {
   return (
